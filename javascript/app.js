59d4fcbb3cc81dd08ba52564c66aa007e93052b4
Connor McKeown <cmckeown@sfu.ca>
2021-08-12 08:56:46 -0700

    uploaded file is now saved in /uploads in the js container

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const formidable = require('formidable')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+// grab uploaded file
+app.post('/upload', (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req);
+
+  form.on('fileBegin', (name, file) => {
+      file.path = __dirname + '/uploads/' + file.name;
+  });
+
+  form.on('file', (name, file) => {
+      console.log('Uploaded ' + file.name);
+  });
+
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(port, () => {
